cfd66da32477f3af983869150f67efed1ed353be
Damien Morlier <morlierdamien35@gmail.com>
2022-10-20 11:52:22 +0200

    Starting to work on the Whole Function Generator + Small fix to scaling and offset

--- assembly/FunctionGenerator.vhd
+++ assembly/FunctionGenerator.vhd
@@ -8,7 +8,7 @@ entity FunctionGenerator is
     port(
     clock               : in std_logic;
     reset               : in std_logic;
-    Frequency           : in std_logic;
+    Frequency           : in integer;
     External            : in std_logic;
     Harmonic            : in std_logic;
     Phase               : in std_logic;
@@ -25,6 +25,11 @@ end FunctionGenerator;
 
 architecture behaviour of FunctionGenerator is
     component DCOPhaser
+        generic(
+            DATA_WIDTH : integer;
+            DATA_SIZE  : integer;
+            CLOCK_FREQ : integer
+        )
         port(
             reset : in std_logic;
             clock : in std_logic;
@@ -33,11 +38,50 @@ architecture behaviour of FunctionGenerator is
         );
     end component;
 
-signal DCO_RAMP : integer :=0;
+    component Offset
+        generic(
+            M : integer;
+            N : integer;
+        )
+        port(
+            RAMP_IN: in std_logic_vector(M-1 downto 0);
+            scaling_value: in std_logic_vector(M-1 downto 0);
+            RAMP_OUT : out std_logic_vector(N-1 downto 0)
+        );
+    
+        
+        
+    end component;
+
+    component scaling
+        generic(
+            M : integer;
+            N : integer;
+        )
+        port(
+            RAMP_IN: in std_logic_vector(M-1 downto 0);
+            offset_value: in std_logic_vector(M-1 downto 0);
+            RAMP_OUT : out std_logic_vector(N-1 downto 0)
+        );
+    end component;
+
+
+
+signal DCO_RAMP : std_logic_vector(4 downto 0);
+signal 
 
     u1 : DCOPhaser port map(
-        reset <= reset
-        clock <= clock
+        reset <= reset,
+        clock <= clock,
         Frequency <= Frequency
+    );
+    u2 : Offset port map(
+        offset_value <= Offset,
+        RAMP_IN <= DCO_RAMP
+    );
 
-    )
+    u3 : scaling port map(
+
+        scaling_value <= scaling,
+        RAMP_IN <= RAMP_SCALED
+    );
--- designs/Offset.vhd
+++ designs/Offset.vhd
@@ -10,10 +10,10 @@ entity offset is
   
   port(
     --input ports
-    input: in std_logic_vector(M-1 downto 0);
+    RAMP_IN: in std_logic_vector(M-1 downto 0);
     offset_value: in std_logic_vector(M-1 downto 0);
     --output ports
-    output: out std_logic_vector(N-1 downto 0)
+    RAMP_OUT: out std_logic_vector(N-1 downto 0)
     
 );
 end offset;
@@ -21,5 +21,5 @@ end offset;
 architecture behave of offset is
  
 begin
-  output <= std_logic_vector(unsigned(input) + unsigned(offset_value));
+  output <= std_logic_vector(unsigned(input) + unsigned(offset_value),N);
 end behave;
