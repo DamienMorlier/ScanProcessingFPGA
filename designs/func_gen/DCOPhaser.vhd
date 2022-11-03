897f3c5a953b4a76d7e1ca39959e9d6fc9bcc269
Damien Morlier <morlierd@DESKTOP-KKQESAC.localdomain>
2022-11-03 08:29:02 +0100

    Finished DCOPhasor, need to write some documentations

--- designs/func_gen/DCOPhaser.vhd
+++ designs/func_gen/DCOPhaser.vhd
@@ -3,44 +3,35 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use work.all;
 
-entity DCOPhaser is 
-    generic( DATA_WIDTH : integer :=5;
-             DATA_SIZE  : integer :=32;
-             CLOCK_FREQ : integer :=1000000 -- 1 Mhz
+entity DCOPhaser is
+    generic(DATA_WIDTH : integer;
+            CLOCK_FREQ : integer
             );
     port(
-        reset       : in std_logic;
-        clock       : in std_logic;
-        Frequency   : in integer;
-        DCO_RAMP    : out std_logic_vector(DATA_WIDTH-1 downto 0)
+        --Frequency : in integer;
+        phase_incr : in std_logic_vector(DATA_WIDTH-1 downto 0);
+        clock     : in std_logic;
+        reset     : in std_logic;
+        DCO_RAMP  : out std_logic_vector(DATA_WIDTH-1 downto 0)
     );
 end DCOPhaser;
+architecture behaviour of DCOPhaser is
+    --constant data_size  : integer := integer(2**DATA_WIDTH);
+    --constant phase_incr : integer := integer(Frequency*data_size/CLOCK_FREQ);
+    signal   count     : unsigned(DATA_WIDTH-1 downto 0 ) := (others => '0');
 
-architecture behaviour of DCOPhaser is 
-constant bit_jump  : integer := integer(CLOCK_FREQ/Frequency/DATA_SIZE);
-signal stored_value, new_stored_value, count,new_count : integer :=0;
+
 begin
-    P_SEQUENTIAL : process(clock)
+    P_SEQUENTIAL : process(clock,reset)
     begin
-        if(rising_edge(clock)) then 
-            stored_value <= new_stored_value;
-            count <= new_count;
+        if (reset = '1') then
+            count <=(others => '0');
         end if;
+        if(rising_edge(clock)) then
+            count <= count + unsigned(phase_incr);
+        end if; 
     end process P_SEQUENTIAL;
 
-    P_COMBINATORIAL : process(stored_value,reset,count)
-    begin
-        if(count = bit_jump) then
-            new_stored_value <= stored_value +1;
-            new_count <= 0;
-        else
-            new_stored_value <=stored_value;
-            new_count <= count +1;
-        end if;
-        if reset = '1' then
-            new_stored_value <= 0;
-            new_count <= 0;
-        end if;
-    end process P_COMBINATORIAL;
-DCO_RAMP <= std_logic_vector(to_unsigned(stored_value,DATA_WIDTH));    
-end behaviour;
+DCO_RAMP <= std_logic_vector(count);    
+
+end behaviour ; -- arch
--- tests/func_gen/benchdcophaser.vhd
+++ tests/func_gen/benchdcophaser.vhd
@@ -9,30 +9,30 @@ end entity DCO_test_bench;
 architecture test of DCO_test_bench is
 
 
-    constant clockcycle : time := 100 ns;
+    constant clockcycle : time := 20 ns;
     constant half_clockcycle : time := clockcycle/2;
 
     component DCOPhaser
     generic(
         DATA_WIDTH : integer;
-        DATA_SIZE  : integer;
         CLOCK_FREQ : integer
     );
     port(
         clock       : in std_logic;
         reset       : in std_logic;
-        frequency   : in integer ;
+        --frequency   : in integer ;
+        phase_incr  : in std_logic_vector(DATA_WIDTH-1 downto 0);
         DCO_RAMP    : out std_logic_vector(DATA_WIDTH-1 downto 0)
     );
     end component;
 
     --Declaration of the internal  signals used in the testbench
-    constant int_DATA_WIDTH : integer := 5;
-    constant int_DATA_SIZE  : integer := 32;
-    constant int_CLOCK_FREQ : integer := 500000;
+    constant int_DATA_WIDTH : integer := 32;
+    constant int_CLOCK_FREQ : integer := 50000000;
     signal int_clock       	    :  std_logic := '0';
 	signal int_reset     		:  std_logic := '0';
-	signal real_frequency 	    :  integer := 1000;
+	--signal real_frequency 	    :  integer := 150000;
+    signal int_phase_incr       : std_logic_vector(int_DATA_WIDTH-1 downto 0) := std_logic_vector(to_signed(integer(15000),int_DATA_WIDTH));
 	signal int_ramp_out      	:  std_logic_vector(int_DATA_WIDTH-1 downto 0);
 
   
@@ -42,13 +42,13 @@ architecture test of DCO_test_bench is
         DUT : DCOPhaser
             generic map(
                 DATA_WIDTH => int_DATA_WIDTH,
-                CLOCK_FREQ => int_CLOCK_FREQ,
-                DATA_SIZE => int_DATA_SIZE
+                CLOCK_FREQ => int_CLOCK_FREQ
             )
             port map (
                 clock           => int_clock,
                 reset           => int_reset,
-                frequency       => real_frequency,
+                phase_incr      => int_phase_incr,
+                --frequency       => real_frequency,
                 DCO_RAMP        => int_ramp_out
                 ) ;
 
@@ -61,10 +61,10 @@ architecture test of DCO_test_bench is
   int_reset <= '1';
   wait for clockcycle;
   int_reset <= '0';
-  wait until int_ramp_out = "00000";
-  wait until int_ramp_out = "11111";
-  wait until int_ramp_out = "00000";
-  wait until int_ramp_out = "11111";
+  wait until to_integer(signed(int_ramp_out)) = 0;
+  wait until to_integer(signed(int_ramp_out)) = 1;
+  wait until to_integer(signed(int_ramp_out)) = 0;
+
   --Raise a deliberate failure to stop execution
   assert false report "TESTBENCH FINISHED, raising a Failure to stop" severity failure;
   end process;
