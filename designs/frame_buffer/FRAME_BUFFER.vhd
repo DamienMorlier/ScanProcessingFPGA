1cd5949d2c3d900d37f66c962c2002b09a3963f6
Campfireman0314 <lqlyd@outlook.com>
2022-11-05 20:01:14 +0100

    Merged from the latest frame_buffer

--- designs/frame_buffer/FRAME_BUFFER.vhd
+++ designs/frame_buffer/FRAME_BUFFER.vhd
@@ -2,6 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use work.all;
+-- use std.env.stop; -- For testbench only
 
 entity FRAME_BUFFER is
 	generic(H_RES: integer := 720 - 1);
@@ -20,10 +21,13 @@ entity FRAME_BUFFER is
 		-- Video frames input, single pixel with 8 bit depth in R-G-B
 		VIDEO_PIXEL_IN: in std_logic_vector(24-1 downto 0);
 		
+		-- Controller for if accepting input data
+		IF_RECV: in std_logic;
+		
 		-- Pixel selector
 		H_IN, V_IN: in unsigned(10-1 downto 0); 
 		
-		-- Zoom parameter, -64 for 0% and 64 for 200% zoom ratio
+		-- Zoom parameter, -100 for 0% and 100 for 200% zoom ratio
 		Zoom: in signed(8-1 downto 0);
 		
 		-- Pixel selector
@@ -47,31 +51,42 @@ end FRAME_BUFFER;
 
 architecture behave of FRAME_BUFFER is
 	-- Constants
-	constant V_RES: integer := 625 - 1; -- 5^4
-	constant H_RATE: integer := 15625; -- 5^5
-	constant V_RATE: integer := 25; -- 5^2
-	-- The highest frequency in this module is 5.5 MHz.
+	constant V_RES: integer := 576 - 1; 
+	constant V_RATE: integer := 25; 
+	-- The highest frequency in this module is 10.368 MHz under PAL standard
 	
 	-- Buses and lines
 	signal reg_write_data: unsigned(24-1 downto 0);
 	signal reg_write_addr, reg_read_addr_A, reg_read_addr_B: unsigned(20-1 downto 0);
 	signal reg_read_internal, reg_read_output: std_logic_vector(24-1 downto 0);
-	signal video_in_rgb_to_intensity: unsigned(8-1 downto 0);
+	signal reg_write_en: std_logic;
+	signal if_new_pixel_available, if_new_pixel_read: std_logic;
 
 begin
 	-- Register
+	-- Refresh the write-in address
+	reg_write_addr <= V_IN + H_IN * V_RES;
+	-- Currently read port A is vacant
+	reg_read_addr_A <= (others => '0');
+	-- Set up read port B for the next pixel
+	reg_read_addr_B <= V_IN + H_IN * V_RES;
+	
+	-- Stop renewing the data when no input clock is detected or IF_RECV is false
+	reg_write_en <= en and if_new_pixel_available and IF_RECV;
+	
+	-- Buffer instantiation
 	reg_line_prefetch : entity work.RegFile(behave)
-	generic map (M => 20, N => 8)
+	generic map (M => 20, N => 24, C => H_RES * V_RES)
 	port map (
 		WD => std_logic_vector(VIDEO_PIXEL_IN),
 		WAddr => std_logic_vector(reg_write_addr),
 		RA => std_logic_vector(reg_read_addr_A),
 		RB => std_logic_vector(reg_read_addr_B),
-		Write => en,
-		ReadA => en,
+		Write => reg_write_en,
+		ReadA => '0', -- Port A not using
 		ReadB => en,
 		reset => reset, 
-		clk => clk_video_pixel_in, 
+		clk => clk, 
 		QA => reg_read_internal,
 		QB => reg_read_output
 	);
@@ -79,7 +94,7 @@ begin
 	-- ReadOut
 	READ_OUT: process(clk, reset, en)
 		variable Xout_temp, Yout_temp: signed(18-1 downto 0); 
-		variable Xclamping, Yclamping: signed(16-1 downto 0);
+		variable Xclamping, Yclamping: integer;
 		variable Rout_temp, Gout_temp, Bout_temp, Iout_temp: unsigned(8-1 downto 0); 
 	begin
 		if(reset = '1') then
@@ -91,15 +106,12 @@ begin
 			Rout <= (others => '0');
 			Gout <= (others => '0');
 			Bout <= (others => '0');
+			if_new_pixel_read <= '0';
+			if_new_pixel_available <= '0';
 		else
-			if(rising_edge(clk) and en = '1') then
+			if(rising_edge(clk) and (en = '1')) then
 				-- Main Process Here --
 				-- Buffering Datapath
-				-- Currently read port A is vacant
-				reg_read_addr_A <= (others => '0');
-				-- Set up read port B for the next pixel
-				reg_read_addr_B <= H_IN + V_IN * V_RES;
-				
 				-- Fetch a new pixel
 				Rout_temp := unsigned(reg_read_output(24-1 downto 16));
 				Gout_temp := unsigned(reg_read_output(16-1 downto 8));
@@ -107,10 +119,19 @@ begin
 				Iout_temp := (Rout_temp + Gout_temp + Bout_temp) / 3;
 				
 				-- Transformation
-				Xout_temp := ( signed(H_IN) - H_RES / 2 ) * Zoom / 100 + signed(H_IN) + signed(H_Position);
-				Yout_temp := ( signed(V_IN) - V_RES / 2 ) * Zoom / 100 + signed(V_IN) + signed(V_Position);
-				Xclamping := H_RES * signed(H_Blanking) / 100;
-				Yclamping := V_RES * signed(V_Blanking) / 100;
+				Xout_temp := ( signed(V_IN) - V_RES / 2 ) * (100 + Zoom) / 100 + V_RES / 2 + V_Position;
+				Yout_temp := ( signed(H_IN) - H_RES / 2 ) * (100 + Zoom) / 100 + H_RES / 2 + H_Position;
+				-- For Debugging
+				report "Xout_temp = "& integer'image(to_integer(Xout_temp));
+				report "Yout_temp = "& integer'image(to_integer(Yout_temp));
+				
+				-- Clamper
+				Xclamping := V_RES * (100 - to_integer(V_Blanking)) / 100;
+				Yclamping := H_RES * (100 - to_integer(H_Blanking)) / 100;
+				-- For Debugging
+				report "Xclamping = "& integer'image(Xclamping);
+				report "Yclamping = "& integer'image(Yclamping);
+				-- stop;
 				
 				-- Clamping and Blanking
 				if (((Xout_temp < 0) or (Xout_temp > H_RES - 1))
@@ -131,6 +152,19 @@ begin
 					Gout <= Gout_temp;
 					Bout <= Bout_temp;
 					Iout <= Iout_temp;
+				end if;
+				
+				if (if_new_pixel_available = '1') then
+					-- Set pixel-is-read flag
+					if_new_pixel_read <= '1';
+					if_new_pixel_available <= '0';
+				else
+					-- Check if new pixel available
+					if(clk_video_pixel_in = '1') then
+						if_new_pixel_available <= '1';
+					else 
+						if_new_pixel_read <= '0';
+					end if;
 				end if;
 			end if;
 		end if;
