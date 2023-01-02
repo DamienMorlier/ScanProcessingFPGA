5f8bb498857db3fe72e858c1d2122886ef70060c
Krakhal <matteo.rod@orange.fr>
2023-01-02 16:36:02 +0100

    merge

--- scripts/osc/test_client.py
+++ scripts/osc/test_client.py
@@ -3,7 +3,7 @@ import random
 
 params = ["/sync", "/harmonic", "/frequency", "/scale", "/phase", "/offset", "/waveform"]
 
-client = SimpleUDPClient("127.0.0.1", 5005)
+client = SimpleUDPClient("192.168.2.99", 5005)
 
 client.send_message("/matrix/F/0", [[50, 100]])
 client.send_message("/generator/229/sync", [[50, 100]])
